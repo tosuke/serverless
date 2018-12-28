b9ac2fa17cf57b79a6cd2a64ddefeafc3860ac2e
Tosuke <tasukeprg@gmail.com>
2018-12-28 11:28:00 +0900

    :gear: add features

--- functions/src/command/gisou.ts
+++ functions/src/command/gisou.ts
@@ -1,24 +1,48 @@
 import { Request } from '../type'
 import { getUserInfo } from '../client'
 
+const opts = {
+  unfurl_links: true,
+  unfurl_media: true,
+  mrkdwn: true
+}
+
+const userRegexp = /<@(\w+)\|[^>]+>(.+)$/
+const emojiRegexp = /(:\w+:)\s*([^\s]+)\s+(.+)$/
+
 export default async (req: Request) => {
-  const regexp = /<@(\w+)\|[^>]+>(.*)$/
-  if (!regexp.test(req.text)) {
-    throw new Error('invalid format')
+  if (userRegexp.test(req.text)) {
+    const [, uid, text] = userRegexp.exec(req.text)
+    return await gisouToUser(req, uid, text)
   }
-  const match = regexp.exec(req.text)
-  const [, uid, text ] = match
+  if (emojiRegexp.test(req.text)) {
+    const [, emoji, name, text] = emojiRegexp.exec(req.text)
+    return await gisouToEmoji(req, emoji, name, text)
+  }
+  throw new Error('invalid format')
+}
+
+async function gisouToUser(req: Request, uid: string, text: string): Promise<any> {
   const user = await getUserInfo(uid)
 
   console.info(`${req.user_name} to ${user.name}`)
 
   const name = user.real_name ? user.real_name : user.profile.display_name
   return {
-    username: name,
+    ...opts,
     icon_url: user.profile.image_192,
-    unfurl_links: true,
-    unfurl_media: true,
-    mrkdwn: true,
-    text: text || ''
+    username: name,
+    text
+  }
+}
+
+async function gisouToEmoji(req: Request, emoji: string, name: string, text: string): Promise<any> {
+  console.info(`${req.user_name} to ${emoji}`)
+
+  return {
+    ...opts,
+    icon_emoji: emoji,
+    username: name,
+    text
   }
 }
